99ce46d1a789fa116331f641741231b098f0fca4
h-bcode <jon.are.br@gmail.com>
2024-06-17 19:15:35 +0200

    Add user.mock.js for mocking user data and authentication response

---
+++ src/scss/user.mock.js
@@ -0,0 +1,12 @@
+export const userData = {
+  name: 'Test User',
+  email: 'test@test.com',
+  password: 'password',
+};
+
+export const accessToken = 'FAKE_ACCESS_TOKEN';
+
+export const authResponse = {
+  ...userData,
+  accessToken,
+};
