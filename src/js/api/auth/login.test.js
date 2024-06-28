e3f5a17c655dbc86afb0b74e8e5d0a84a6a90443
h-bcode <jon.are.br@gmail.com>
2024-06-28 20:01:10 +0200

    Add email validation for login functionality and seperate users in user.mock.js

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,8 +1,9 @@
-/* eslint-env jest */
+/* UNIT testing */
 
 import * as mocks from '../../../mocks';
 import { login } from './login';
 import { load } from '../../storage/';
+import { validateEmail } from './emailvalidator.js';
 
 const userResponseClone = () => ({ ...mocks.authResponse });
 
@@ -18,12 +19,27 @@ describe('login', () => {
   });
 
   it('should set the token with a successful login', async () => {
-    await login(mocks.userData.email, mocks.userData.password);
+    await login(mocks.validUserData.email, mocks.validUserData.password);
     expect(load('token')).toBe(mocks.accessToken);
   });
 
   it('should return the user name with a successful login', async () => {
-    const result = await login(mocks.userData.email, mocks.userData.password);
-    expect(result.name).toBe(mocks.userData.name);
+    const result = await login(
+      mocks.validUserData.email,
+      mocks.validUserData.password,
+    );
+    expect(result.name).toBe(mocks.validUserData.name);
+  });
+});
+
+describe('email validation', () => {
+  it('should reject invalid email format', () => {
+    const invalidEmail = mocks.invalidUserData.email;
+    expect(validateEmail(invalidEmail)).toBe(false);
+  });
+
+  it('should accept valid email format', () => {
+    const validEmail = mocks.validUserData.email;
+    expect(validateEmail(validEmail)).toBe(true);
   });
 });
--- src/mocks/user.mock.js
+++ src/mocks/user.mock.js
@@ -1,12 +1,18 @@
-export const userData = {
-  name: 'Test User',
-  email: 'test@test.com',
-  password: 'password',
+export const validUserData = {
+  name: 'Valid User',
+  email: 'validUser@stud.noroff.no',
+  password: 'password!23',
+};
+
+export const invalidUserData = {
+  name: 'Invalid User',
+  email: 'invalidUser@gmail.com',
+  password: 'password323',
 };
 
 export const accessToken = 'FAKE_ACCESS_TOKEN';
 
 export const authResponse = {
-  ...userData,
+  ...validUserData,
   accessToken,
 };
