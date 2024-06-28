a40e23eedc7baa747bcf6f080e75ca2dafc5132c
h-bcode <jon.are.br@gmail.com>
2024-06-28 20:02:15 +0200

    Refactor logout test script. added login function and added "remove profile test" to check if token and profil are "null"

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -2,17 +2,38 @@
 
 import * as mocks from '../../../mocks';
 import { logout } from './logout';
+import { login } from './login';
+import { load } from '../../storage/';
+
+const userResponseClone = () => ({ ...mocks.authResponse });
 
 describe('logout', () => {
   beforeEach(() => {
+    global.fetch = mocks.createMockFetch(userResponseClone());
     global.localStorage = mocks.localStorageMock();
   });
   afterEach(() => {
     global.localStorage.clear();
   });
+
+  it('should set the token with a successful login', async () => {
+    await login(mocks.validUserData.email, mocks.validUserData.password);
+    expect(load('token')).toBe(mocks.accessToken);
+  });
+
+  it('should return the user name with a successful login', async () => {
+    const result = await login(
+      mocks.validUserData.email,
+      mocks.validUserData.password,
+    );
+    expect(result.name).toBe(mocks.validUserData.name);
+  });
+
   it('clears the token from localStorage', () => {
     localStorage.setItem('token', mocks.accessToken);
+    localStorage.setItem('profile', mocks.authResponse);
     logout();
     expect(localStorage.getItem('token')).toBe(null);
+    expect(localStorage.getItem('profile')).toBe(null);
   });
 });
